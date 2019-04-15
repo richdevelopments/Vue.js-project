56f6b8c7cf00d918a478f64102da46905d1ed807
Richard Wood <richardwood5@hotmail.com>
2019-04-15 20:33:57 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image:'/images/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+    }
+    
+});
+
+variants: [
+    {
+        variantId: 2234,
+        variantColor: "green"
+    },
+    {
+        variantId: 2235,
+        variantColor: "blue"
+    }
+    
+    ];
+    
